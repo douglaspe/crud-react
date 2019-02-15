497b0d3a903fc4ba43ddeacc8ced7ed6a67bf5f1
Douglas <douglas@gmail.com>
2019-02-15 15:15:59 -0200

    Criando componente Home

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio"
+        subtitle="Segundo Projeto do capítulo de React.">
+        <div className="dislpay-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+    </Main>
