b41b2b185064b9be07890e780736e8a5ffb1e442
Douglas <douglas@gmail.com>
2019-04-03 15:24:43 -0300

    Update logo

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router';
 
 import Home from '../components/home/Home';
 import UserPage from "../components/user/UserPage";
@@ -8,5 +8,5 @@ export default () =>
     <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/users' component={UserPage} />
-        <Route path='/users/:id' component={UserPage} />
+        <Redirect from='*' to='/' />
     </Switch>
